e5e93a438cda40aefe5b014a6c9c475967795077
teradaian <teradaian@gmail.com>
2021-08-20 15:16:24 -0700

    add feeding form

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 # import your models here
-from .models import Bat
+from .models import Bat, Feeding
 
 # Register your models here
 admin.site.register(Bat)
+admin.site.register(Feeding)
+
