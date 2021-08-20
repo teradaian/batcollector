7cb130c02168adf12c84131cd3dda3d237a607f7
teradaian <teradaian@gmail.com>
2021-08-20 15:15:59 -0700

    add feeding view

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
 class Bat(models.Model):
   name = models.CharField(max_length=100)
@@ -13,4 +18,18 @@ class Bat(models.Model):
   
   def get_absolute_url(self):
       return reverse("bat_details", kwargs={"pk": self.pk})
+  
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  bat = models.ForeignKey(Bat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
   
