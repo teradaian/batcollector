33b33021ebee7cebf12cbfa15122397f40cd04ff
teradaian <teradaian@gmail.com>
2021-08-20 15:29:33 -0700

    add_feeding view done

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bat
 from .forms import FeedingForm
@@ -30,3 +30,11 @@ def bat_details(request, bat_id):
   bat = Bat.objects.get(id=bat_id)
   feeding_form = FeedingForm()
   return render(request, 'bats/detail.html', { 'bat': bat, 'feeding_form': feeding_form })
+
+def add_feeding(request, bat_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.bat_id = bat_id
+    new_feeding.save()
+  return redirect('bats_detail', bat_id=bat_id)
