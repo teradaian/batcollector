6c7e569d3683c586ab637f62e48d7d488c781b27
teradaian <teradaian@gmail.com>
2021-08-20 15:28:27 -0700

    provide form action

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
   path('bats/create/', views.BatCreate.as_view(), name='bats_create'),
   path('bats/<int:pk>/update/', views.BatUpdate.as_view(), name='bats_update'),
   path('bats/<int:pk>/delete/', views.BatDelete.as_view(), name='bats_delete'),
+  path('bats/<int:bat_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
 
 
