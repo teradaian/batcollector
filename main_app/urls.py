d321c8efbf0ebfc0beb4a91a6aac758b7a1fd807
teradaian <teradaian@gmail.com>
2021-08-20 10:00:20 -0700

    add create route to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name="about"),
   path('bats/', views.bats_index, name='bats_index'),
-  path('bats/<int:bat_id>/', views.bat_details, name='bat_details')
+  path('bats/<int:bat_id>/', views.bat_details, name='bat_details'),
+  path('bats/create/', views.BatCreate.as_view(), name='bats_create'),
+  
 ]
+
 
