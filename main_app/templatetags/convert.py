e54f355398fb47fba3583b22ee9502362bea4c7d
teradaian <teradaian@gmail.com>
2021-08-22 12:35:02 -0700

    add color name from hexpicker

--- main_app/templatetags/convert.py
+++ main_app/templatetags/convert.py
@@ -3,14 +3,16 @@ from django import template
 
 register = template.Library()
 
-@register.simple_tag
 def closest_colour(requested_colour):
+    print(requested_colour)
+    s = requested_colour[4:]
+    req_color = eval(s)
     min_colours = {}
     for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - (requested_colour[0])) ** 2
-        gd = (g_c - (requested_colour[1])) ** 2
-        bd = (b_c - (requested_colour[2])) ** 2
+        rd = (r_c - (req_color[0])) ** 2
+        gd = (g_c - (req_color[1])) ** 2
+        bd = (b_c - (req_color[2])) ** 2
         min_colours[(rd + gd + bd)] = name
     return min_colours[min(min_colours.keys())]
 
@@ -18,5 +20,3 @@ def closest_colour(requested_colour):
 def get_colour_name(requested_colour):
     closest_name = closest_colour(requested_colour)
     return closest_name
-
-# print(get_colour_name((139,0,0)))
